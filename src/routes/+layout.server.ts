471046fac12bb66f2aa99588fcecc6d8899cfbb4
Chris Antreasian <chrisantreasian@gmail.com>
2022-10-08 00:34:21 -0400

    move logic to layout

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,9 @@
 import type { LayoutServerLoad } from './$types';
-import type { PageDetails, StrapiApiResp } from "$lib/types";
+import type { PageDetails, StrapiApiResp, StrapiBase } from "$lib/types";
 
-import { error } from '@sveltejs/kit';
+import { error, type Cookies } from '@sveltejs/kit';
 import { mkRequest, handleGetResponse, queryStr } from "$lib/api";
+import * as crypto from "node:crypto"
 
 const pQ =  queryStr({ 
   filters: { id: { $in: 6 } },
@@ -13,23 +14,70 @@ const pQ =  queryStr({
 	]
 });
 
-const mkMobileString = (_: string) => {
+const daysFromNow = (_: number) => new Date(new Date().getTime()+(_*24*60*60*1000))
+
+const setDistinctId = (arr: string[]) => {
+  const dId = crypto.randomBytes(20).toString('hex');
+  if (arr.includes(dId)) {
+    setDistinctId(arr);
+  } else {
+    return dId
+  }
+}
+const shortenString = (_: string) => {
 	const a = _.split(" ")
 	return `${a[0].charAt(0)}. ${a[1]}`
 }
 
-export const load: LayoutServerLoad = async () => {
-	const response = await mkRequest("GET", `page-slugs?${pQ}`);	
-	const res = await handleGetResponse(response);
-	if (res.ok) {		
-		const details: StrapiApiResp<PageDetails> = await res.json();
+export const handleCookie = async (cookies: Cookies) => {
+  console.log("THIS cookie h  IS CALLED")
+  
+	const cid = cookies.get("distinctId");
+  if (cid) {
+		console.log("cookie already set"); 
+		return;
+	};
+  
+  const response = await mkRequest("GET", `mixpanel-distinct-ids?${queryStr({ populate: "*" })}`);
+  const res = await handleGetResponse(response);
+
+  if (res.ok) {
+    const resp: StrapiApiResp<StrapiBase & {distinctId: string}> = await res.json();
+    const idsArr = resp.data.map(_ => _.attributes.distinctId);
+
+		const did = setDistinctId(idsArr);
+    cookies.set("distinctId", did, {
+      httpOnly: true,
+      expires: daysFromNow(400)
+    })
+    const request = await mkRequest(
+      "POST", 
+      `mixpanel-distinct-ids`, 
+      { data: { distinctId: did } }
+    );
+    console.log("post result", did);	
+  }
+}
+
+
+export const load: LayoutServerLoad = async ({ cookies}) => {
+	
+	const cid = cookies.get("distinctId");
+
+	const getData = await mkRequest("GET", `page-slugs?${pQ}`);	
+	const pageData = await handleGetResponse(getData);
+	
+	await handleCookie(cookies);
+	
+	if (pageData.ok) {		
+		const pageDetails: StrapiApiResp<PageDetails> = await pageData.json();
 		return { 
-			logo: details.data[0].attributes.image,
-			title: details.data[0].attributes.title,
-			mobileTitle: mkMobileString(details.data[0].attributes.title),
+			logo: pageDetails.data[0].attributes.image,
+			title: pageDetails.data[0].attributes.title,
+			mobileTitle: shortenString(pageDetails.data[0].attributes.title),
 		};
 	}
 
-	const { message } = await res.json();
+	const { message } = await pageData.json();
 	throw error(500, message);
 }
--- src/routes/api/mixpanel/collect/+server.ts
+++ src/routes/api/mixpanel/collect/+server.ts
@@ -2,12 +2,12 @@ import * as mixpanel from "mixpanel";
 
 const mxpInst = mixpanel.init(import.meta.env.VITE_MIXPANEL_PROJECT_TOKEN);
 
-export async function POST({ request }) {
+export async function POST({ request, cookies}) {
   const { eKey, props }: { eKey: string, props: any } = await request.json();
   mxpInst.track(eKey, {
     ...props,
     time: new Date(),
-    distinct_id: "passed from sesion",
+    distinct_id: cookies.get("distinctId"),
   });
   return new Response()
 }
