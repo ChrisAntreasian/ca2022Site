e6fde01aca027b5d57d1cab7be21bf0d868ebc69
Chris Antreasian <chrisantreasian@gmail.com>
2022-11-18 23:17:58 -0500

    some styles

--- src/lib/BuildConfirm.svelte
+++ src/lib/BuildConfirm.svelte
@@ -6,13 +6,21 @@
 <section>
   <h3>{title} Built Successfully</h3>
   <pre> 
+    <code>
     {JSON.stringify(data, undefined, 2)}
+    </code>
   </pre>
 </section>
 
 <style>
   section {
-    margin-top: 1rem;
+    flex-direction: column;
+    margin-top: 4rem;
+    padding: 0 1rem 3rem;
+    box-sizing: border-box;
+  }
+  h3 {
+    text-align: center;
   }
 
 </style>
