3bf49e6fd2a94d402c5af675fcff1fe5766113d8
Chris Antreasian <chrisantreasian@gmail.com>
2023-02-07 01:35:44 -0500

    remove console.log e Please enter the commit message for your changes. Lines starting

--- src/routes/poems/[...pid]/+page.server.ts
+++ src/routes/poems/[...pid]/+page.server.ts
@@ -6,9 +6,8 @@ export const load: PageServerLoad = async ({ params }) => {
 	const d = D.data;
 	const pid = parseInt(params.pid) || d.data[Math.floor(Math.random() * d.data.length)].id;
 	const poem = d.data.filter((_: WithId<Poem>) => _.id === pid)[0];
-	console.log({...d, data: d.data.sort((a, b) => a.attributes.position - b.attributes.position)})
 	return {
-		poems: {...d, data: d.data.sort(_ => _.attributes.position)},
+		poems: {...d, data: d.data.sort((a, b) => a.attributes.position - b.attributes.position)},
 		poem
 	};
 };
