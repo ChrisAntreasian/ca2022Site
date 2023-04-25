1795cf760d2a5bdc03bbf88017028b4e4ea8f693
Chris Antreasian <chrisantreasian@gmail.com>
2023-04-24 23:27:16 -0400

    untested task either

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 import { error } from "@sveltejs/kit";
+import { pipe } from "fp-ts/function";
 import * as qs from "qs";
-
+import * as TE from "fp-ts/TaskEither"
 type QuryProps = {
 	filters?: Record<string, any>,
 	populate?: string | string[]
@@ -10,8 +11,19 @@ export const baseApi = import.meta.env.VITE_BASE_API;
 
 export const queryStr = (_: QuryProps) => qs.stringify(_, { encodeValuesOnly: true });
 
+export const fetchRequestB =  (urlBase: string) => async (method: "GET" | "POST", resource: string, data?: Record<string, unknown>) => await pipe(
+	TE.tryCatch(
+		() => fetch(`${urlBase}/api/ {resource}`, {
+			method,
+			headers: { "content-type": "application/json" },
+			body: data && JSON.stringify(data)
+		}),
+		(reason) => new Error(`${reason}`),
+	),
+)()
+
 export const fetchRequest = (urlBase: string) => async (method: "GET" | "POST", resource: string, data?: Record<string, unknown>) => {
-	const d = await fetch(`${urlBase}/api/${resource}`, {
+	const d = await fetch(`${urlBase}/api/ {resource}`, {
 		method,
 		headers: {
 			'content-type': 'application/json'
