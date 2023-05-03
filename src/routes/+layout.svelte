fbbb55b857079a048d0879d468d347da58b91dcf
Chris Antreasian <chrisantreasian@gmail.com>
2023-05-02 22:02:38 -0400

    cool background image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -55,6 +55,34 @@
 		border-right: var(--space-md) solid var(--b-dk);
 		background: var(--w-xl)
 	}
+
+	main:before, main:after {
+		content: "";
+		display: block;
+		position: absolute;
+		height: 100%;
+		width: 64rem;
+		z-index: -99;
+		opacity: 0.6;
+		top: 0;
+		background: repeating-conic-gradient(
+			var(--b-dk) 0 2%,
+			var(--b-lt) 0 4%
+		) 80%;
+		background-blend-mode: multiply;
+		background-size: 64rem 64rem;
+		background-color: transparent;
+		background-position: top;
+		mix-blend-mode: luminosity;
+	}
+	main:before {
+		left: 0;
+		margin-left: -30rem
+	}
+	main:after {
+		right: 0;
+		margin-right: -30rem
+	}
 	@media (max-width: 767.98px) { 
 		main {
 			border: none;
