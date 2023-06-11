50f04707e08c432f464791647ec338e65c613809
Chris Antreasian <chrisantreasian@gmail.com>
2023-06-11 01:48:52 -0400

    boop

--- src/lib/build.ts
+++ src/lib/build.ts
@@ -1,13 +1,8 @@
 
 import { E, T, TE, FN, RA, RR } from "$lib/fp-ts";
 
-import { writeFsTE, type RouteKeyU } from '$lib/file';
-import { throwErrIO, type HttpErrE, type HttpErrTE, e403 } from "./error";
-
-type FetchKey<A> = HttpErrTE<{
-  key: HttpErrE<string>;
-  data: A;
-}>
+import { writeFsTE } from '$lib/file';
+import { throwErrIO, type HttpErrTE, e403 } from "./error";
 
 const { VITE_BUILD_KEY, VITE_ENV } = import.meta.env;
 
@@ -18,24 +13,12 @@ export const buildGate = FN.flow(
   ),
   TE.fromEither,
 );
-
-export const fetchKey = <A>(
-  buildKey: HttpErrE<RouteKeyU>,
-  fetchFn: HttpErrTE<A>
-): FetchKey<A> => FN.pipe(
-  fetchFn, 
-  TE.map(_ => ({
-    key: buildKey, 
-    data: _
-  })),
-);
   
 export const writeFile = <A>(buildKey: string) => TE.chain((_: A) => writeFsTE<A>([_, buildKey]));
   
 export const combineResp: (kd: HttpErrTE<ReadonlyArray<RR.ReadonlyRecord<string, any>>>) => HttpErrTE<RR.ReadonlyRecord<string, any>> = TE.map(RA.reduce({}, (acc, curr) => ({...acc, ...curr})));
 
-type BuildFn<A, B> = (ma: HttpErrTE<A>) => HttpErrTE<B>
-export const buildRes = <A, B>(title: string, bFn: BuildFn<A, B>) => FN.flow(
+export const buildRes = <A, B>(title: string, bFn:  (ma: HttpErrTE<A>) => HttpErrTE<B>) => FN.flow(
   buildGate,
   bFn,
   TE.fold(throwErrIO(), FN.flow(_ => ({ title: title, data: _}), T.of))
